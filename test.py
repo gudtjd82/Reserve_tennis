998f069b047a6ea33eace703b946789bb28ad0bb
gudtjd82 <gudtjd82@naver.com>
2023-09-10 11:55:54 +0900

    시간 설정 전

---
+++ test.py
@@ -0,0 +1,12 @@
+import pyperclip
+import pyautogui as pg
+import time
+import sqlite3
+import os
+import re
+import subprocess
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
